147728ce4d77d3710f4255d76032f64e300c38a2
tian junjie <dajay520@gmail.com>
2012-01-10 00:36:07 +0800

    完善列表显示

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
   
   validates_presence_of :title,:description, :image_url
   validates_numericality_of :price
+  validates_length_of :title, :within => 10..100, :on => :create, :message => "must be present"
   
   
   

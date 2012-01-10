b260e90819f1dd7e9a5009e586bfcf66776fffca
tian junjie <dajay520@gmail.com>
2012-01-11 00:44:15 +0800

    增加购物车

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,19 @@ class Product < ActiveRecord::Base
   validates_numericality_of :price
   validates_length_of :title, :within => 10..100, :on => :create, :message => "must be present"
   
+  has_many :line_items
+  before_destroy  :ensure_not_referenced_by_any_line_item
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base,'Line Items present')
+      return false
+      
+    end
+    
+  end
   
   
 end
